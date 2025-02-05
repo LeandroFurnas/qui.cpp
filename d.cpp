fae15e18a1378a45b8584014d063425baf16ffe1
LeandroFurnas <furnas2008@gmail.com>
2025-02-05 00:58:31 +0000

    Update d.cpp

--- d.cpp
+++ d.cpp
@@ -14,17 +14,15 @@ int main() {
 
     string frase_oculta = frase;
 
+    // Oculta a frase com '_', mantendo os espaços intactos
     for (char &l : frase_oculta) {
         if (l != ' ') {
             l = '_';
         }
     }
 
-    for (char c : frase_oculta) {
-        cout << c << " ";
-    }
-
-    cout << "\n\n";
+    // Exibe a frase oculta
+    cout << frase_oculta << "\n\n";
 
     while (true) {
         cout << "Tente adivinhar a frase!" << endl;
@@ -33,6 +31,7 @@ int main() {
 
         bool acertou = false;
 
+        // Verifica se a letra inserida está na frase
         for (size_t i = 0; i < frase.size(); i++) {
             if (frase[i] == letra) {
                 frase_oculta[i] = letra;
@@ -44,10 +43,12 @@ int main() {
             cout << "A letra '" << letra << "' não está na frase. Tente novamente!\n";
         }
 
-        system("cls"); // Limpa a tela após o jogador inserir uma letra
+        system("cls");  // Limpa a tela após a tentativa
 
+        // Exibe a frase oculta atualizada
         cout << frase_oculta << "\n\n";
 
+        // Se a frase oculta for igual à frase original, o jogador venceu
         if (frase_oculta == frase) {
             system("cls");
             cout << "\nParabéns! Você adivinhou a frase: " << frase << endl;
